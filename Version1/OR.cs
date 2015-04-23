88564698a5b62d4a9c9d24a184f8372c4a8ad941
Edwin Hattink <edwinhattink@me.com>
2015-04-23 09:38:52 +0200

    VersionOneDiagram

--- Version1/OR.cs
+++ Version1/OR.cs
@@ -2,9 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Version1
 {
-    
+    public class OR : IComponent
+    {
+    }
 }
