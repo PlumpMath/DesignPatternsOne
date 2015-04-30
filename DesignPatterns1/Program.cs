449e012fba91e075527ce7c311f3547a3982cac3
Yannik Hegge <yannikhegge@gmail.com>
2015-04-30 08:54:43 +0200

    everything

--- DesignPatterns1/Program.cs
+++ DesignPatterns1/Program.cs
@@ -10,32 +10,8 @@ namespace DesignPatterns1
     {
         static void Main(string[] args)
         {
-            /*
-            Source src = new Source();
-            Source src2 = new Source();
-            Not i= new Not();
-            AND and = new AND();
-
-            and.addInput(src);
-            i.addInput(src2);
-            and.addInput(i);
-
-            Console.WriteLine("pre-trigger");
-            src.printState();
-            src2.printState();
-            i.printState();
-            and.printState();
-
-            Console.WriteLine("post-trigger");
-            src.trigger();
-            src.activate(true);
-            src2.activate(false);
-            src.printState();
-            src2.printState();
-            i.printState();
-            and.printState();
-            */
-            Import();
+            
+            Program.Import();
             Console.ReadLine();
         }
 
